a5f59398d28d8df0806066144e516a02a381bc89
Kamil1812 <kamilniewiadomski9@gmail.com>
2022-01-27 15:02:19 +0100

    Koniec

--- app/src/main/java/com/example/zadaniezdalne/MainActivity.kt
+++ app/src/main/java/com/example/zadaniezdalne/MainActivity.kt
@@ -2,10 +2,64 @@ package com.example.zadaniezdalne
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CheckedTextView
+import android.widget.TextView;
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+            val P1View = findViewById<CheckedTextView>(R.id.P1)
+
+            if (P1View.isChecked)
+            {
+                 findViewById<TextView>(R.id.ChlebText).text = "Chleb znajduje się na twojej liście zakupów"
+            }
+            else
+            {
+                findViewById<TextView>(R.id.ChlebText).text = "Nie jest to na twojej liście zakupów."
+            }
+
+            val P2View = findViewById<CheckedTextView>(R.id.P2)
+
+            if (P2View.isChecked)
+            {
+                findViewById<TextView>(R.id.ChlebText).text = "Mleko znajduje się na twojej liście zakupów"
+            }
+
+            else
+            {
+                findViewById<TextView>(R.id.ChlebText).text = "Nie jest to na twojej liście zakupów."
+            }
+
+
+            val P3View = findViewById<CheckedTextView>(R.id.P3)
+
+            if (P3View.isChecked)
+            {
+                findViewById<TextView>(R.id.ChlebText).text = "Masło znajduje się na twojej liście zakupów"
+            }
+            else
+            {
+                findViewById<TextView>(R.id.ChlebText).text = "Nie jest to na twojej liście zakupów."
+            }
+
+
+            val P4View = findViewById<CheckedTextView>(R.id.P4)
+
+            if (P4View.isChecked)
+            {
+                findViewById<TextView>(R.id.ChlebText).text = "Herbata znajduje się na twojej liście zakupów"
+            }
+            else
+            {
+                findViewById<TextView>(R.id.ChlebText).text = "Nie jest to na twojej liście zakupów."
+            }
+
+        }
     }
-}
+
+
